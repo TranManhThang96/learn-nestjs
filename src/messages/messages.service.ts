762d68867dab5d052b906ce4a1dee4fc7160ad06
tranmanhthang96nd <tranmanhthang@cryptopie-labo.com>
2022-07-19 15:01:43 +0700

    implementing a service

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { MessageRepository } from './messages.repository';
 
 @Injectable()
-export class MessagesService {}
+export class MessagesService {
+  messageRepo: MessageRepository;
+
+  constructor() {
+    // Service is creating its own dêpndencies
+    // DONT DO THIS ON REAL APPS
+    this.messageRepo = new MessageRepository();
+  }
+
+  findOne(id: string) {
+    return this.messageRepo.findOne(id);
+  }
+
+  findAll() {
+    return this.messageRepo.findAll();
+  }
+
+  create(message: string) {
+    return this.messageRepo.create(message);
+  }
+}
