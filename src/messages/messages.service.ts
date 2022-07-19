1c2bb3742365241d6f7e27fb460832eb2c274c65
tranmanhthang96nd <tranmanhthang@cryptopie-labo.com>
2022-07-19 17:54:09 +0700

    organizing code with modules

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -9,7 +9,20 @@ export class MessagesService {
   //   this.messageRepo = messageRepo;
   // }
 
-  constructor(public messageRepo: MessagesRepository) {}
+  constructor(
+    public messageRepo: MessagesRepository,
+    public messageRepo2: MessagesRepository,
+    public messageRepo3: MessagesRepository,
+    public messageRepo4: MessagesRepository,
+  ) {
+    console.log(messageRepo === messageRepo2); // true
+    console.log(messageRepo3 === messageRepo4); // true
+    console.log(
+      messageRepo3, // MessagesRepository {}
+      typeof messageRepo3, // object
+      messageRepo3 instanceof MessagesRepository, // true
+    );
+  }
 
   findOne(id: string) {
     return this.messageRepo.findOne(id);
