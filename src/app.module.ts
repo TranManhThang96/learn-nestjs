e1dd19cd04aaf1fdd102869dd1cfb07df2f4cbe0
tranmanhthang96nd <tranmanhthang@cryptopie-labo.com>
2022-07-22 10:29:41 +0700

    creating an entity and repository

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessagesModule } from './messages/messages.module';
@@ -6,9 +7,26 @@ import { ComputerModule } from './computer/computer.module';
 import { CpuModule } from './cpu/cpu.module';
 import { DiskModule } from './disk/disk.module';
 import { PowerModule } from './power/power.module';
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
+import { User } from './users/user.entity';
 
 @Module({
-  imports: [MessagesModule, ComputerModule, CpuModule, DiskModule, PowerModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User],
+      synchronize: true,
+    }),
+    MessagesModule,
+    ComputerModule,
+    CpuModule,
+    DiskModule,
+    PowerModule,
+    UsersModule,
+    ReportsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
